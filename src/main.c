c9f2ae9623f481c4a2ba296f29ca7d027d48491c
firasmlt <mellitifiras@protonmail.com>
2025-11-23 05:17:06 +0100

    adding echo exit and clear commands

--- src/main.c
+++ src/main.c
@@ -3,6 +3,20 @@
 #include <string.h>
 #include <wchar.h>
 #include <unistd.h>
+#define MAXTOKENS 100;
+
+size_t tokenizeCommands(char* buf, char** tokens){
+  size_t tokens_len = 0;
+
+  char* token = strtok(buf, " ");
+
+  while(token != NULL){
+    tokens[tokens_len] = strdup(token);
+    tokens_len++;
+    token = strtok(NULL, " ");
+  }
+  return tokens_len;
+}
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
@@ -27,15 +41,33 @@ int main(int argc, char *argv[]) {
 
     if(buf[0] == '\0') continue;
 
+    char** tokens = malloc(100 * sizeof(char*));
+    size_t tokens_len = tokenizeCommands(buf, tokens);
 
+    if(tokens_len < 1) continue;
 
-    if(strcmp(buf, "clear") == 0){
-      write(1, "\033[2J\033[H", 7);
+    // CLEAR COMMAND
+    if(strcmp(tokens[0], "clear") == 0){
+      if(tokens_len == 1){
+        write(1, "\033[2J\033[H", 7);
+      }else{
+        printf("clear: Unknown arguments provided; try 'clear' instead\n");
+      }
       continue;
     }
-
-    if(strcmp(buf, "exit") == 0){
+    // EXIT COMMAND
+    if(strcmp(tokens[0], "exit") == 0 && tokens_len == 1){
       break;
+    }
+
+    // ECHO COMMAND
+    if(strcmp(tokens[0], "echo") == 0){
+      for(size_t i = 1; i < tokens_len; i++){
+        if(i!=1) printf(" ");
+        printf("%s", tokens[i]);
+      }
+      printf("\n");
+      continue;
     }
 
 
